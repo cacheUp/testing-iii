5bfb64db1aad8426a034150098af1d5c96084b49
cacheUp <bradb26@gmail.com>
2019-03-07 01:54:01 +0300

    MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,32 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+import Dashboard from '../dashboard/Dashboard';
+
+afterEach(cleanup);
+
+describe('The Controls Component', () => {
+    test('renders the Control component', () => {
+        render(<Controls />)
+    })
+
+    test('has toggle buttons', () => {
+        const { getByText } = render(<Controls closed={false} locked={true} />);
+        getByText(/lock gate/i);
+        getByText(/close gate/i);
+    })
+    
+    test('cant disable when open and unlocked', () => {
+        const { getByText } = render(<Controls closed={false} locked={false} />);
+        const toggleLockButton = getByText(/lock gate/i);
+        expect(toggleLockButton).toHaveAttribute('disabled', '');
+    })
+
+    test('cant close when already closed', () => {
+        const { getByText } = render(<Controls closed={true} />);
+        getByText(/open gate/i);
+    })
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
 // Test away
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Dashboard from './Dashboard';
+
+describe('Dashboard renders', () => {
+    test('shows dashboard', () => {
+        render(<Dashboard />)
+    })
+})
